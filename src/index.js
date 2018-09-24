0f2d7f50933110289cb6e34d56c76adf52dffaed
KukuruzaAndrey <1!1Andreyiloveandrey>
2018-09-25 02:21:55 +0300

    worked app

--- src/index.js
+++ src/index.js
@@ -1,35 +1,53 @@
 import simpleGit from 'simple-git/promise';
+import showdown from 'showdown';
+import fs from 'fs';
 
 const keyWord = 'CHANGELOG.md';
 const startChar = '+';
 const git = simpleGit(__dirname);
+
 git.log()
-    .then(log => log.all.map(comm => comm.hash))
-    .then((hash) => git.diff([hash[15], hash[0]]))
-    .then(diff => console.log(filterChangelogFromDiff(diff)));
+    .then(log => log.all.map(commit => commit.hash))
+    .then((commitHashes) => git.diff([commitHashes[15], commitHashes[0]]))
+    .then(diff => buildData(diff))
+    .then(changes => renderOutput(changes))
+    .then(html => fs.appendFile('global-changelog.html', html, 'utf8', (err) => {
+        if (err) throw err;
+        console.log('The "data to append" was appended to file!');
+    }));
 
-const filterChangelogFromDiff = diff => {
-    const filesDiff = diff.split('diff --git');
-    const filtered = filesDiff
-        .filter(fileDiff => fileDiff.split('\n')[0].includes(keyWord))
-        .map(fileDiff => fileDiff
-            .split('\n')
-            .filter(line => line.includes(keyWord) || line.startsWith(startChar)));
-    return filtered;
+
+const buildData = (rawDiff) => {
+    const filterChangelogFromDiff = diff => {
+        const filesDiff = diff.split('diff --git');
+        const filtered = filesDiff
+            .filter(fileDiff => fileDiff.split('\n')[0].includes(keyWord))
+            .map(fileDiff => fileDiff
+                .split('\n')
+                .filter(line => line.includes(keyWord) || line.startsWith(startChar)));
+        return filtered;
+    };
+    const fileChange = fileDiff => {
+        const packageName = fileDiff[0].match(/packages\/(.+?)\//)[1];
+        const changes = fileDiff.slice(3).map(line => line.substring(1)).join('\n');
+        return {
+            packageName,
+            changes
+        }
+    };
+    return new Promise((resolve, reject) => {
+        const filteredDiff = filterChangelogFromDiff(rawDiff);
+        resolve(filteredDiff.map(fileDiff => fileChange(fileDiff)));
+    });
 };
 
-const buildData = (diff) => {
-
-    const [head, ...tail] = diff;
-
+const renderOutput = data => {
+    return new Promise((resolve, reject) => {
+        const mdConverter = new showdown.Converter();
+        const output = data.map(obj => {
+            return [`<h2>Package ${obj.packageName}</h2>`,
+                mdConverter.makeHtml(obj.changes)].join('\n')
+        }).join('<br>');
+        resolve(output);
+    })
 };
-
-const genChange = fileDiff => {
-    const packageName = fileDiff[0].match(/packages\/(.+?)\//)[0];
-    const changes = fileDiff.slice(3).map(line => line.substring(1));
-    return {
-        packageName,
-        changes
-    }
-};
-//return line.includes(keyWord) || line.startsWith(startChar);
