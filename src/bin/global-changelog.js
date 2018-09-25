08435027790d85a64cded96f84bc7a58eaae8163
KukuruzaAndrey <andreyku6@gmail.com>
2018-09-25 13:03:53 +0300

    fix

--- src/bin/global-changelog.js
+++ src/bin/global-changelog.js
@@ -4,7 +4,7 @@ import program from 'commander'
 import generateChangelog from '..'
 
 program
-    .version('0.2.3')
+    .version('0.2.1')
     .description('Generate changelog of changelogs for some time period')
     .option('-c, --commit', 'With commit tag')
     .action(() => {
